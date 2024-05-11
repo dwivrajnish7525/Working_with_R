f84b7e6150d34eb5beb36ca7f078f6f3be23fd14
Rajnish Dwivedi <rajnishdwivediofficial6@gmail.com>
2024-05-11 20:31:00 +0530

    Update 33.Boxplots.R

--- 33.Boxplots.R
+++ 33.Boxplots.R
@@ -11,7 +11,7 @@ boxplot(x,y)
 x=c(1,2,3,3,4,5,5,7,9,9,15,25)
 boxplot(x,horizontal=TRUE)
 
-#For adding colors,Labels and graph name.
+#For adding colors,Labels and plot name.
 x=c(1,2,3,4,5,6,7,8,9,10,11,12)
 y=c(5,7,8,6,3,4,1,5,2,3,6,4,2,0,1)
 a=c(1,2,3,6,6,6,7,8,9)
