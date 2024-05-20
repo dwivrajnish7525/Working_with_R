0919f2288ef48c17a0fecb9e39996f541875a310
Rajnish Dwivedi <rajnishdwivediofficial6@gmail.com>
2024-05-20 21:23:19 +0530

    Update 32.Histograms.r

--- 32.Histograms.r
+++ 32.Histograms.r
@@ -1,6 +1,12 @@
 # Create data for the graph.
 v <- c(19, 23, 11, 4, 16, 21, 32,14, 19, 27, 39)
-# Create the histogram.
-hist(v, xlab = "No.of Articles ", col = "yellow", border = "red",xlim=c(0,40),ylim = c(0,5),breaks = 5)
 
+# Create the histogram.
+hist(v)
+
+#Adding color in histogram.
+hist(v,col = "yellow", border = "red")
+
+#Adding labels in histogram.
+hist(v, xlab = "No.of Articles ", col = "yellow", border = "red",xlim=c(0,40),ylim = c(0,5),breaks = 5)
 
