f71d6b19a9b03d31f0c4979a7ac47658ea610cd3
Rajnish Dwivedi <rajnishdwivediofficial6@gmail.com>
2024-05-20 21:18:25 +0530

    Update 33.Boxplots.R

--- 33.Boxplots.R
+++ 33.Boxplots.R
@@ -11,8 +11,12 @@ boxplot(x,y)
 x=c(1,2,3,3,4,5,5,7,9,9,15,25)
 boxplot(x,horizontal=TRUE)
 
-#For adding colors,Labels and plot name.
+#For adding Labels and graph name.
 x=c(1,2,3,4,5,6,7,8,9,10,11,12)
 y=c(5,7,8,6,3,4,1,5,2,3,6,4,2,0,1)
 a=c(1,2,3,6,6,6,7,8,9)
+boxplot(x,y,a,horizontal=TRUE,names=c("Level 1","Level 2","Level 3"))
+
+#For adding colors in boxplot.
 boxplot(x,y,a,horizontal=TRUE,names=c("Level 1","Level 2","Level 3"),col=c("Orange","White","Green"))
+
