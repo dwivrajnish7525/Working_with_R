b4eaa8469b280d1a7b5258cfa9e866c9bed261ab
Rajnish Dwivedi <rajnishdwivediofficial6@gmail.com>
2024-05-20 21:29:30 +0530

    Update 30.Dot Plot.r

--- 30.Dot Plot.r
+++ 30.Dot Plot.r
@@ -3,4 +3,7 @@ categories <- c("A", "B", "C", "D")
 values <- c(10, 20, 15, 25)
 
 # Create dot chart
+dotchart(values,labels = categories)
+
+#Adding Color and title in dot chart.
 dotchart(values, labels = categories, col="red",main = "Dot Chart Example", xlab = "Values")
